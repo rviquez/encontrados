c85b987b75043c2d652e29d1f530f367d018f2e8
Roberto Viquez <robertoviquez@gmail.com>
2019-12-06 08:01:55 +0100

    Amplify and vuetify setup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import Amplify from 'aws-amplify'
+import {
+  components
+} from 'aws-amplify-vue'
+import AwsExports from './aws-exports'
 
 Vue.config.productionTip = false
+
+Amplify.configure(AwsExports)
 
 new Vue({
   router,
   store,
+  vuetify,
+  components: {
+    ...components
+  },
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,78 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import {
+  components,
+  AmplifyEventBus,
+  AmplifyPlugin
+} from 'aws-amplify-vue'
+import * as AmplifyModules from 'aws-amplify'
+import AmplifyStore from '../store/index'
 
 Vue.use(VueRouter)
+Vue.use(AmplifyPlugin, AmplifyModules)
+
+let user
+
+getUser().then((user, error) => {
+  if (user) {
+    router.push({
+      path: '/'
+    })
+  }
+})
+
+AmplifyEventBus.$on('authState', async (state) => {
+  if (state === 'signedOut') {
+    user = null
+    AmplifyStore.commit('setUser', null)
+    router.push({
+      path: '/auth'
+    })
+  } else if (state === 'signedIn') {
+    user = await getUser()
+    router.push({
+      path: '/'
+    })
+  }
+})
+
+function getUser () {
+  return Vue.prototype.$Amplify.Auth.currentAuthenticatedUser().then((data) => {
+    if (data && data.signInUserSession) {
+      AmplifyStore.commit('setUser', data)
+      return data
+    }
+  }).catch((e) => {
+    AmplifyStore.commit('setUser', null)
+    return null
+  })
+}
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/about',
     name: 'about',
+    meta: {
+      requiresAuth: true
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/auth',
+    name: 'Authenticator',
+    component: components.Authenticator
   }
 ]
 
@@ -24,6 +80,22 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeResolve(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    user = await getUser()
+    if (!user) {
+      return next({
+        path: '/auth',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    }
+    return next()
+  }
+  return next()
 })
 
 export default router
